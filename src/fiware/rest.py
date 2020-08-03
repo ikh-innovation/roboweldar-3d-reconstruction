d3d4ef21fdd65f4deb774141298f2d08af0e3686
Orfeas Kypris <okypris@iknowhow.com>
2020-08-03 16:28:54 +0300

    add template data model and example

--- src/fiware/rest.py
+++ src/fiware/rest.py
@@ -1,2 +1,18 @@
 import pyfiware
 
+
+
+oc = pyfiware.OrionConnector("http://127.0.0.1:1026")
+print(oc)
+
+
+result = oc.count()
+result = oc.get("123")
+
+# create service
+
+result = oc.create()
+
+
+print(result)
+
