34d25e3d08681d5d0a7e162bbcc77afaacb03078
Orfeas Kypris <okypris@iknowhow.com>
2020-09-07 18:04:58 +0300

    transform computed poses to real poses

--- src/pose_input/plot_poses.py
+++ src/pose_input/plot_poses.py
@@ -8,6 +8,30 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+def rotation_matrix_x(theta):
+    return np.array(
+        [[1, 0.0, 0.0],
+         [0.0, np.cos(theta), -np.sin(theta)],
+         [0.0, np.sin(theta), np.cos(theta)]]
+    )
+
+
+def rotation_matrix_y(theta):
+    return np.array(
+        [[np.cos(theta), 0.0, np.sin(theta)],
+         [0.0, 1.0, 0.0],
+         [-np.sin(theta), 0.0, np.cos(theta)]]
+    )
+
+
+def rotation_matrix_z(theta):
+    return np.array(
+        [[np.cos(theta), -np.sin(theta), 0.0],
+         [np.sin(theta), np.cos(theta), 0.0],
+         [0.0, 0.0, 1.0]]
+    )
+
+
 class Pose:
     def __init__(self, id, pos_vec, rot_mat):
         self.id = id
@@ -47,15 +71,44 @@ def plot_camera_poses(ax, poses, color):
     plt.axis("auto")
 
 
-def optimal_rotation(r_reals, r_computeds, Omega):
+def optimal_translation(r_reals, r_computeds, r_estimate):
     l = []
-    O = Omega.reshape(4, 4)
-    O[2, 3] = 0.0
-    O[3, 0:3] = 0.0
-    O[3, 3] = 1.0
-    print(O)
+
+    # print(r_estimate)
     for i, (r_real, r_computed) in enumerate(zip(r_reals, r_computeds)):
-        l.append(np.linalg.norm(r_real - np.matmul(O, r_computed)))
+        l.append(np.linalg.norm(r_real - r_estimate))
+    return np.sum(l)
+
+
+def get_3d_rotation_matrix(alpha, beta, gamma):
+    return rotation_matrix_x(alpha) @ rotation_matrix_y(beta) @ rotation_matrix_z(gamma)
+
+
+def get_scaling_matrix(a, b, c):
+    return np.array([
+        [a, 0.0, 0.0],
+        [0.0, b, 0.0],
+        [0.0, 0.0, c], ]
+    )
+
+
+def optimal_rotation(r_reals, r_computeds, rotation_angles):
+    l = []
+    alpha, beta, gamma = rotation_angles[0], rotation_angles[1], rotation_angles[2]
+    S = get_3d_rotation_matrix(alpha, beta, gamma)
+    # print(S)
+    for i, (r_real, r_computed) in enumerate(zip(r_reals, r_computeds)):
+        l.append(np.linalg.norm(r_real - np.matmul(S, r_computed)))
+    return np.sum(l)
+
+
+def optimal_scaling(r_reals, r_computeds, scaling_factors):
+    l = []
+    a, b, c = scaling_factors[0], scaling_factors[1], scaling_factors[2]
+    scaling = get_scaling_matrix(a, b, c)
+    # print(scaling)
+    for i, (r_real, r_computed) in enumerate(zip(r_reals, r_computeds)):
+        l.append(np.linalg.norm(r_real - np.matmul(scaling, r_computed)))
     return np.sum(l)
 
 
@@ -63,27 +116,118 @@ def order_poses_by_id(poses):
     return sorted(poses, key=lambda x: x.id)
 
 
-def optimize(real_poses, computed_poses):
-    x0 = np.random.uniform(0, 1, [1, 16])
+def optimize_rotation(real_poses, computed_poses):
+    x0_Omega = np.random.uniform(-2 * np.pi, +2 * np.pi, [1, 3])
     ordered_real_poses = order_poses_by_id(real_poses)
     ordered_computed_poses = order_poses_by_id(computed_poses)
-    r_reals = [np.concatenate([pose.pos_vec, np.array([[1]])]) for pose in ordered_real_poses]
-    r_computeds = [np.concatenate([pose.pos_vec, np.array([[1]])]) for pose in ordered_computed_poses]
-    partial_optimal_rotation = partial(optimal_rotation, r_reals, r_computeds)
-    res = minimize(partial_optimal_rotation, x0, method='Nelder-Mead')
-    Omega = np.array(res.x).reshape(4, 4)
+
+    print(compute_geometric_center(ordered_real_poses))
+    print(compute_geometric_center(ordered_computed_poses))
+
+    r_reals = [pose.pos_vec for pose in ordered_real_poses]
+    r_computeds = [pose.pos_vec for pose in ordered_computed_poses]
+
+    partial_func_optimal_rotation = partial(optimal_rotation, r_reals, r_computeds)
+    res2 = minimize(partial_func_optimal_rotation, x0_Omega, method='Nelder-Mead', tol=1e-12)
+    alpha = res2.x[0]
+    beta = res2.x[1]
+    gamma = res2.x[2]
+    Omega = get_3d_rotation_matrix(alpha, beta, gamma)
+    print(alpha, beta, gamma)
 
     return Omega
 
 
-def transform_computed_poses(Omega, computed_poses):
-    l = []
-    for pose in computed_poses:
-        pose.pos_vec = np.matmul(Omega, np.concatenate([np.array(pose.pos_vec).reshape(3, 1), np.array([[1]])]))
+def optimize_scaling(real_poses, computed_poses):
+    x0_scaling = np.random.uniform(-5, 5, [1, 3])
+    ordered_real_poses = order_poses_by_id(real_poses)
+    ordered_computed_poses = order_poses_by_id(computed_poses)
 
-    transformed_poses = computed_poses
+    # print(compute_geometric_center(ordered_real_poses))
+    # print(compute_geometric_center(ordered_computed_poses))
+
+    r_reals = [pose.pos_vec for pose in ordered_real_poses]
+    r_computeds = [pose.pos_vec for pose in ordered_computed_poses]
+
+    partial_func_optimal_scaling = partial(optimal_scaling, r_reals, r_computeds)
+    res2 = minimize(partial_func_optimal_scaling, x0_scaling, method='Nelder-Mead', tol=1e-12)
+    a = res2.x[0]
+    b = res2.x[1]
+    c = res2.x[2]
+
+    # print(a, b, c)
+    return get_scaling_matrix(a, b, c)
+
+
+def transform_poses(scaling, Omega, r, poses):
+    l = []
+    for pose in poses:
+        pose.pos_vec = pose.pos_vec - r
+        pose.pos_vec = scaling @ Omega @ pose.pos_vec
+
+    transformed_poses = poses
 
     return transformed_poses
+
+
+def compute_geometric_center(poses):
+    xs = np.mean([pose.pos_vec[0] for pose in poses])
+    ys = np.mean([pose.pos_vec[1] for pose in poses])
+    zs = np.mean([pose.pos_vec[2] for pose in poses])
+    return np.array([xs, ys, zs]).reshape(3, 1)
+
+
+def translate_poses(computed_poses, r_estimated):
+    c = deepcopy(computed_poses)
+    for pose in c:
+        pose.pos_vec -= r_estimated
+
+    return c
+
+
+def check_omega(Omega: np.ndarray):
+    print(np.matmul(Omega, np.transpose(Omega)))
+
+
+def center_poses(poses):
+    centroid = compute_geometric_center(poses)
+    centered_poses = translate_poses(poses, centroid)
+    return centered_poses, centroid
+
+
+def pipeline(real_poses, computed_poses):
+    centered_real_poses, real_centroid = center_poses(real_poses)
+    centered_computed_poses, computed_centroid = center_poses(computed_poses)
+
+    Omega = optimize_rotation(centered_real_poses, centered_computed_poses)
+    check_omega(Omega)
+    transformed_poses = transform_poses(scaling=np.identity(3), Omega=Omega, r=computed_centroid,
+                                        poses=deepcopy(computed_poses))
+
+    scaling = optimize_scaling(centered_real_poses, transformed_poses)
+
+    doubly_transformed_poses = transform_poses(scaling=scaling, Omega=Omega, r=np.array([0.0, 0.0, 0.0]).reshape(3, 1),
+                                               poses=deepcopy(transformed_poses))
+
+    # rotate again
+
+    Omega2 = optimize_rotation(centered_real_poses, doubly_transformed_poses)
+    triply_transformed_poses = transform_poses(scaling=np.identity(3), Omega=Omega2, r=np.array([0.0, 0.0, 0.0]).reshape(3, 1),
+                                        poses=deepcopy(doubly_transformed_poses))
+
+
+    # scale again
+    scaling2 = optimize_scaling(centered_real_poses, triply_transformed_poses)
+
+    quadruply_transformed_poses = transform_poses(scaling=scaling2, Omega=np.identity(3),
+                                               r=np.array([0.0, 0.0, 0.0]).reshape(3, 1),
+                                               poses=deepcopy(triply_transformed_poses))
+
+    for t_pose, dt_pose in zip(transformed_poses, doubly_transformed_poses):
+        print("before scaling: {}".format(t_pose.pos_vec))
+        print("after scaling: {}".format(dt_pose.pos_vec))
+
+    return quadruply_transformed_poses
 
 
 def main():
@@ -98,14 +242,18 @@ def main():
     computed_poses = extract_inferred_camera_poses(load_computed_poses(
         path_to_cameras_sfm="/mnt/storage/roboweldar/simulation_test_1/MeshroomCache/StructureFromMotion/578f830addc4cce0c6fccaca48fd23068ffff1a3/cameras.sfm"))
 
+    transformed_poses = pipeline(real_poses, computed_poses)
+
+    # plot_func(ax, [Pose(None, computed_gc, None)], 'y')
+    # plot_func(ax, [Pose(None, real_gc, None)], 'y')
+    # plot_func(ax, translate_poses(computed_poses, computed_gc), 'g')
+    # plot_func(ax, translate_poses(real_poses, real_gc), 'r')
+
     plot_func(ax, real_poses, 'r')
     plot_func(ax, computed_poses, 'g')
-
-    Omega = optimize(real_poses, computed_poses)
-    print(Omega)
-    transformed_poses = transform_computed_poses(Omega, deepcopy(computed_poses))
-
     plot_func(ax, transformed_poses, 'b')
+    plot_func(ax, center_poses(real_poses)[0], 'm')
+
     plt.show()
 
 
