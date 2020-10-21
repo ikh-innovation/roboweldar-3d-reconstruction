ff4ec27e5127b0035f3800adab56a72c922f5c09
thanos <tsakelliou@iknowhow.com>
2020-10-21 11:59:10 +0300

    DockerFile and requirements.

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 ROOT_DIR = os.path.split(os.path.abspath(__file__))[0]
-MESHROOM_DIR = os.path.join(ROOT_DIR, "deps", "Meshroom-2019.2.0")
+MESHROOM_DIR = os.path.join("/opt", "Meshroom-2019.2.0")
 IMAGES_DIR = os.path.join(ROOT_DIR, "reconstruction_data", "raw")
 OUTPUT_DIR = os.path.join(ROOT_DIR, "reconstruction_data", "output")
 CACHE_DIR = os.path.join(ROOT_DIR, "reconstruction_data", "cache")
